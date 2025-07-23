4e1148e505add4fa1ac8c10ef7020ddd69108fcd
Ankit Sharma <ankitjaimini01@gmail.com>
2025-07-23 22:28:12 +0530

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import FeaturedDestination from '../components/FeaturedDestination'
+import ExclusiveOffers from '../components/ExclusiveOffers'
+import Testimonial from'../components/Testimonial'
+import NewsLetter from '../components/NewsLetter'
+
+const Home = () => {
+  return (
+    <>
+      <Hero >
+      </Hero> 
+      <FeaturedDestination>
+      </FeaturedDestination>
+      <ExclusiveOffers>
+      </ExclusiveOffers>
+      <Testimonial>
+      </Testimonial>
+      <NewsLetter>
+      </NewsLetter>
+      
+    </>
+  )
+}
+
+export default Home
